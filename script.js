f9bc027a82f32d752b60266e1323a296cac509ae
Yukihan <yukanzen@hotmail.com>
2023-10-02 13:25:11 -0400

    Day selection added

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+// CHART
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+
+
+
+
+
+
+
+// API REQUEST
+
 const url = 'https://api.open-meteo.com/v1/forecast?';
 const latitude = 48.8534;
 const longitude = 2.3488;
